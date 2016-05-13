c415853e42768323b166d054c50aec824ab6fb5b
Tiberiu Tofan <tibtof@gmail.com>
2016-05-13 22:36:58 +0300

    refactoring

---
+++ src/test/java/java8playground/OptionalByDesignTest.java
@@ -0,0 +1,123 @@
+package java8playground;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
+
+public class OptionalByDesignTest {
+
+    public static final String EVENT_WITHOUT_LOCATION = "NewConcertInTown";
+    public static final String MISSING_EVENT = "NoEvent";
+    public static final String EVENT_IN_BUCHAREST = "EventInBucharest";
+    private EventRepository eventRepository;
+
+    @Before
+    public void setup() {
+        eventRepository = Mockito.mock(EventRepository.class);
+        when(eventRepository.findEvent(anyString())).thenReturn(Optional.empty());
+        when(eventRepository.findEvent(eq(EVENT_WITHOUT_LOCATION))).thenReturn(Optional.of(new Event()));
+        when(eventRepository.findEvent(eq(EVENT_IN_BUCHAREST)))
+                .thenReturn(Optional.of(new Event(new Location(new Address(new City(Locations.BUCHAREST))))));
+    }
+
+    @Test
+    public void noEvent() throws Exception {
+        assertEquals(Locations.TBA, getEventCity(MISSING_EVENT));
+    }
+
+    @Test
+    public void noLocation() throws Exception {
+        assertEquals(Locations.TBA, getEventCity(EVENT_WITHOUT_LOCATION));
+    }
+
+    @Test
+    public void validLocation() throws Exception {
+        assertEquals(Locations.BUCHAREST, getEventCity(EVENT_IN_BUCHAREST));
+    }
+
+    private String getEventCity(String eventId) {
+        return eventRepository.findEvent(eventId)
+                .flatMap(Event::getLocation)
+                .map(Location::getAddress)
+                .map(Address::getCity)
+                .map(City::getName)
+                .orElse(Locations.TBA);
+    }
+
+    public static class EventRepository {
+
+        private Map<String, Event> events = new HashMap<>();
+
+        public Optional<Event> findEvent(String eventId) {
+            return Optional.ofNullable(events.get(eventId));
+        }
+    }
+
+    public static class Event {
+
+        private Location location;
+
+        public Event(Location location) {
+            this.location = location;
+        }
+
+        public Event() {
+        }
+
+        public Optional<Location> getLocation() {
+            return Optional.ofNullable(location);
+        }
+
+    }
+
+    public static class Location {
+        private Address address;
+
+        public Location(Address address) {
+            this.address = address;
+        }
+
+        public Address getAddress() {
+            return address;
+        }
+    }
+
+    public static class Address {
+        private City city;
+
+        public Address(City city) {
+            this.city = city;
+        }
+
+        public City getCity() {
+            return city;
+        }
+    }
+
+    public static class City {
+        private String name;
+
+        public City(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    class Locations {
+        public static final String TBA = "TBA";
+        public static final String BUCHAREST = "Bucharest";
+    }
+
+}
---
+++ src/test/java/java8playground/OptionalLegacyTest.java
@@ -0,0 +1,70 @@
+package java8playground;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static java8playground.OptionalLegacyTest.Category.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class OptionalLegacyTest {
+
+    @Test
+    public void noDrivingLicence() {
+        assertFalse(canDriveSmallVehicle(new Person(null)));
+    }
+
+    @Test
+    public void bCategoryDrivingLicence() {
+        assertTrue(canDriveSmallVehicle(new Person(new DrivingLicence(A, B))));
+    }
+
+    @Test
+    public void noBCategoryDrivingLicence() {
+        assertFalse(canDriveSmallVehicle(new Person(new DrivingLicence(C))));
+    }
+
+    private Boolean canDriveSmallVehicle(Person person) {
+        return Optional.ofNullable(person)
+                .map(Person::getDrivingLicence)
+                .map(DrivingLicence::getCategories)
+                .map(categories -> categories.contains(B))
+                .orElse(Boolean.FALSE);
+    }
+
+    enum Category {
+        A, B, C, D
+    }
+
+    static class Person {
+
+        private DrivingLicence drivingLicence;
+
+        public Person(DrivingLicence drivingLicence) {
+            this.drivingLicence = drivingLicence;
+        }
+
+        public DrivingLicence getDrivingLicence() {
+            return drivingLicence;
+        }
+    }
+
+    static class DrivingLicence {
+
+        private Set<Category> categories;
+
+        public DrivingLicence(Category... categories) {
+            this.categories = new HashSet<>(Arrays.asList(categories));
+        }
+
+        public Set<Category> getCategories() {
+            return categories;
+        }
+    }
+
+
+}
