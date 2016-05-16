8e7f32e5200a36e9cd1de5801b477e2b980b471c
Tiberiu Tofan <tibtof@gmail.com>
2016-05-16 22:04:50 +0300

    - split into multiple methods - grouping example

--- src/test/java/java8playground/StreamTest.java
+++ src/test/java/java8playground/StreamTest.java
@@ -1,6 +1,6 @@
 package java8playground;
 
-import org.junit.Assert;
+import java8playground.streams.Page;
 import org.junit.Test;
 
 import java.math.BigInteger;
@@ -13,174 +13,186 @@ import static java.util.Collections.synchronizedList;
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.*;
 import static java8playground.streams.PageCollector.toPage;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class StreamTest {
 
-	private List<Integer> numbers = Arrays.asList(1, 6, 5, 3, 4, 2, 8, 1, 5, 7, 4);
-	private List<Person> persons = createPersonList();
+    private List<Integer> numbers = Arrays.asList(1, 6, 5, 3, 4, 2, 8, 1, 5, 7, 4);
+    private List<Person> persons = createPersonList();
 
-	private static List<Person> createPersonList() {
-		return Arrays.asList(
-				new Person("Robert", "Martin", 1952),
-				new Person("Martin", "Fowler", 1963),
-				new Person("James", "Gosling", 1955),
-				new Person("Brendan", "Eich", 1961),
-				new Person(null, null, null),
-				new Person("Kent", "Beck", 1961),
-				new Person("Joshua", "Bloch", 1961),
-				new Person(null, null, null)
-		);
-	}
+    private static List<Person> createPersonList() {
+        return Arrays.asList(
+                new Person("Robert", "Martin", 1952),
+                new Person("Martin", "Fowler", 1963),
+                new Person("James", "Gosling", 1955),
+                new Person("Brendan", "Eich", 1961),
+                new Person("Kent", "Beck", 1961),
+                new Person("Joshua", "Bloch", 1961)
+        );
+    }
 
-	public void test() {
-		persons.stream().sorted(comparing(Person::getYearOfBirth)).forEach(System.out::println);
-		persons.stream().map(p -> p.getFirstName() + " " + p.getLastName())
-				.forEach(System.out::println);
-		persons.stream()
-				.filter(p -> now().getYear() - p.getYearOfBirth() < 50)
-				.count();
-		persons.stream().map(Person::getYearOfBirth).distinct()
-				.forEach(System.out::println);
-		persons.stream().map(p -> now().getYear() - p.getYearOfBirth())
-				.sorted().findFirst()
-				.ifPresent(System.out::println);
+    @Test
+    public void anyMatch() {
+        boolean areThereAnyPersonsWithK = persons.stream()
+                .map(Person::getFirstName)
+                .anyMatch(n -> n.startsWith("K"));
 
-//        numbers.stream().filter(i -> i % 2 != 0).forEach(System.out::println);
-//        numbers.stream().filter(i -> i % 2 != 0).distinct().forEach(System.out::println);
-//        numbers.stream().filter(i -> i % 2 == 0).limit(3).forEach(System.out::println);
-//        numbers.stream().filter(i -> i % 2 == 0).skip(3)
-		Integer pageOffset = 0;
-		Integer pageSize = 0;
-		persons.stream().skip(pageOffset).limit(pageSize).collect(toPage(pageOffset, pageSize, persons.size()));
+        assertTrue(areThereAnyPersonsWithK);
+    }
 
+    @Test
+    public void allMatch() {
+        boolean areAllPeopleOlderThan50 = persons.stream()
+                .map(Person::getYearOfBirth)
+                .allMatch(y -> now().getYear() - y > 50);
 
-//        products.stream().skip((currentPage - 1) * pageSize).limit(pageSize)
-//                .forEach(System.out::println);
+        assertTrue(areAllPeopleOlderThan50);
+    }
 
-		List<String> words = Arrays.asList("Java", "Scala", "JavaScript", "Kotlin");
-		words.stream().parallel()
-				.map(word -> word.split(""))
-				.flatMap(Arrays::stream)
-				.distinct()
-				.sequential()
-				.forEach(System.out::print);
-		//outputs: JavSclriptKon
+    @Test
+    public void infiniteRandom() {
+        Stream.generate(Math::random)
+                .map(n -> n * 100)
+                .map(Double::intValue)
+                .limit(10)
+                .forEach(System.out::println);
+    }
 
+    @Test
+    public void parallel() {
+        List<String> words = Arrays.asList("Java", "Scala", "JavaScript", "Kotlin");
 
-		Stream.generate(Math::random)
-				.map(n -> n * 100)
-				.limit(10)
-				.forEach(System.out::println);
+        words.stream().parallel()
+                .map(word -> word.split(""))
+                .flatMap(Arrays::stream)
+                .distinct()
+                .forEach(System.out::print);
+    }
 
-		boolean areThereAnyPersonsWithK = persons.stream()
-				.map(Person::getFirstName)
-				.anyMatch(n -> n.startsWith("K"));
+    @Test
+    public void pagination() {
+        Page<Person> personPage = persons.stream().skip(1).limit(4).collect(toPage(1, 4, persons.size()));
 
-		boolean areAllPeopleYoungerThan50 = persons.stream()
-				.map(Person::getYearOfBirth)
-				.allMatch(y -> now().getYear() - y < 50);
-		System.out.println(areThereAnyPersonsWithK);
+        assertEquals(4, personPage.getCurrentPage().size());
+    }
 
-		Optional<Integer> findAnyPrime = numbers.stream().map(BigInteger::valueOf)
-				.filter(i -> i.isProbablePrime(Integer.MAX_VALUE))
-				.findFirst().map(BigInteger::intValue);
-		System.out.print(findAnyPrime.get());
-	}
+    @Test
+    public void countUnder55() {
+        assertEquals(1,
+                persons.stream()
+                        .filter(p -> now().getYear() - p.getYearOfBirth() < 55)
+                        .count());
+    }
 
-	@Test
-	public void indexById() {
-		Map<String, String> map = persons.stream().collect(toMap(Person::getId, Person::getFirstName));
-		System.out.println(map.getClass());
-	}
+    @Test
+    public void sortByYearOfBirth() {
+        persons.stream().sorted(comparing(Person::getYearOfBirth)).forEach(System.out::println);
+    }
 
-	@Test
-	public void primeNumbers() {
-		Stream.iterate(0, n -> n + 1).map(BigInteger::valueOf).filter(i -> {
-			System.out.println();
-			return i.isProbablePrime(Integer.MAX_VALUE);
-		}).limit(10).forEach(System.out::println);
-	}
+    @Test
+    public void indexById() {
+        Map<String, String> map = persons.stream().collect(toMap(Person::getId, Person::getFirstName));
+        System.out.println(map.getClass());
+    }
 
-	@Test
-	public void generateFirstPrimeNumbers() {
-		List<Integer> accumulator =
-				Stream.iterate(0, n -> n + 1).parallel()
-						.map(BigInteger::valueOf)
-						.filter(i -> i.isProbablePrime(Integer.MAX_VALUE))
-						.limit(100_000)
-						.map(BigInteger::intValue)
-						.collect(Collectors.toList());
+    @Test
+    public void primeNumbers() {
+        Stream.iterate(0, n -> n + 1).map(BigInteger::valueOf).filter(i -> {
+            System.out.println();
+            return i.isProbablePrime(Integer.MAX_VALUE);
+        }).limit(10).forEach(System.out::println);
+    }
 
-		Assert.assertEquals(100_000, accumulator.size());
-//		return accumulator;
-	}
+    @Test
+    public void generateFirstPrimeNumbers() {
+        List<Integer> accumulator =
+                Stream.iterate(0, n -> n + 1).parallel()
+                        .map(BigInteger::valueOf)
+                        .filter(i -> i.isProbablePrime(Integer.MAX_VALUE))
+                        .limit(100_000)
+                        .map(BigInteger::intValue)
+                        .collect(Collectors.toList());
 
-	public List<Person> personsUnderAge(int age) {
-		return persons.stream().filter(p -> now().getYear() - p.getYearOfBirth() < 50)
-				.collect(toCollection(() -> synchronizedList(new ArrayList<>())));
-	}
+        assertEquals(100_000, accumulator.size());
+    }
 
-	@Test
-	public void personsStartingWithA() {
-		List<String> personsWithA = persons.stream()
-				.map(Person::getFirstName)
-				.map(Optional::ofNullable)
-				.map(n -> n.orElse("Kappa"))
-				.filter(n -> n.startsWith("K"))
-				.collect(collectingAndThen(toList(), Collections::unmodifiableList));
+    @Test
+    public void personsUnderAge() {
+        List<Person> personsUnder50 = persons.stream().filter(p -> now().getYear() - p.getYearOfBirth() < 50)
+                .collect(toCollection(() -> synchronizedList(new ArrayList<>())));
 
-		System.out.println(personsWithA);
-	}
+        personsUnder50.forEach(System.out::println);
+    }
 
-	@Test
-	public void reduceSum() {
-		Integer sum = numbers.stream().reduce(0, (a, b) -> a + b);
-//		Integer sum = numbers.stream().reduce(0, Integer::sum);
-		System.out.println(sum);
-	}
+    @Test
+    public void personsStartingWithA() {
+        List<String> personsWithA = persons.stream()
+                .map(Person::getFirstName)
+                .map(Optional::ofNullable)
+                .map(n -> n.orElse("Kappa"))
+                .filter(n -> n.startsWith("K"))
+                .collect(collectingAndThen(toList(), Collections::unmodifiableList));
 
-	@Test
-	public void reduceMax() {
-		Optional<Integer> max = numbers.stream().reduce(Integer::max);
-		max.ifPresent(System.out::println);
-	}
+        System.out.println(personsWithA);
+    }
 
-	static class Person {
-		private String id = UUID.randomUUID().toString();
-		private String firstName;
-		private String lastName;
-		private Integer yearOfBirth;
+    @Test
+    public void reduceSum() {
+//        Integer sum = numbers.stream().reduce(0, (a, b) -> a + b);
+        Integer sum = numbers.stream().reduce(0, Integer::sum);
+        System.out.println(sum);
+    }
 
-		public Person(String firstName, String lastName, Integer yearOfBirth) {
-			this.firstName = firstName;
-			this.lastName = lastName;
-			this.yearOfBirth = yearOfBirth;
-		}
+    @Test
+    public void reduceMax() {
+        Optional<Integer> max = numbers.stream().reduce(Integer::max);
+        max.ifPresent(System.out::println);
+    }
 
-		public String getId() {
-			return id;
-		}
+    @Test
+    public void countByYearOfBirth() {
+        Map<Integer, Long> countByYear = persons.stream().sorted(comparing(Person::getYearOfBirth)).collect(groupingBy(Person::getYearOfBirth, counting()));
 
-		public String getFirstName() {
-			return firstName;
-		}
+        countByYear.forEach((year, count) -> System.out.println(year + " " + count));
+    }
 
-		public String getLastName() {
-			return lastName;
-		}
+    static class Person {
+        private String id = UUID.randomUUID().toString();
+        private String firstName;
+        private String lastName;
+        private Integer yearOfBirth;
 
-		public Integer getYearOfBirth() {
-			return yearOfBirth;
-		}
+        public Person(String firstName, String lastName, Integer yearOfBirth) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.yearOfBirth = yearOfBirth;
+        }
 
-		@Override
-		public String toString() {
-			return "Person{" +
-					"firstName='" + firstName + '\'' +
-					", lastName='" + lastName + '\'' +
-					", yearOfBirth=" + yearOfBirth +
-					'}';
-		}
-	}
+        public String getId() {
+            return id;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public Integer getYearOfBirth() {
+            return yearOfBirth;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", yearOfBirth=" + yearOfBirth +
+                    '}';
+        }
+    }
 
 }
